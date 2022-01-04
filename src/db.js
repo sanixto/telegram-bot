f23edecf6048a8d0cfe488e8611f8ae16304c13d
sanixto <sanix.to@gmail.com>
2022-01-05 01:37:54 +0200

    init sequelize database

---
+++ src/db.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const { DB_HOST } = process.env;
+const { DB_PORT } = process.env;
+const { DB_USER } = process.env;
+const { DB_PASS } = process.env;
+const { DB } = process.env;
+
+module.exports = new Sequelize(DB, DB_USER, DB_PASS, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: "mysql",
+});
