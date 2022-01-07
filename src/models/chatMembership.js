cca72d8ebd8f2c8ecb0232b8801941fc34b587fc
sanixto <sanix.to@gmail.com>
2022-01-07 21:19:21 +0200

    create model chatMembeshipr

---
+++ src/models/chatMembership.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+const ChatMembership = sequelize.define(
+  "chatMembership",
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    chatId: {
+      type: DataTypes.STRING,
+      foreignKey: true,
+    },
+    right: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    wrong: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+  }
+);
+
+module.exports = ChatMembership;
--- src/models/models.js
+++ src/models/models.js
@@ -1,7 +1,9 @@
 const User = require("./user");
 const Chat = require("./chat");
+const ChatMembership = require("./chatMembership");
 
 module.exports = {
   UserModel: User,
   ChatModel: Chat,
+  ChatMembershipModel: ChatMembership,
 };
